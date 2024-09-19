d306312471d975b52cbd784abe15361844664ebe
Mariano Cardoso <marianocardoso92@gmail.com>
2024-09-19 00:12:05 -0300

    primera subida

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace EJERCICIO_2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //PASO 0: DECLARACION DE VARIABLES
+            //TIPO DE DATO: INT, FLOAT, CHAR, BOOL
+            int N1, N2, resultado;
+
+            //PASO 1: PEDIR VALORES
+            Console.WriteLine("INGRESE UN NUMERO: ");
+            N1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("INGRESE OTRO NUMUERO: ");
+            N2 = int.Parse(Console.ReadLine());
+            
+            //PASO 2: REALIZAR CALCULO
+            resultado = N1 + N2;
+
+            //PASO 3: EMITIR RESULTADO
+            Console.WriteLine("EL RESULTADO ES: " + resultado);
+        }
+    }
+}
